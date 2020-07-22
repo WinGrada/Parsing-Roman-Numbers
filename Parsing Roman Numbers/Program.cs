090105e446025c99a903bcdc4a297786b552b7bc
Wingrada <wingrada@gmail.com>
2020-07-22 17:01:26 +0500

    Написал примерный план как реализовать решение задачи

--- Parsing Roman Numbers/Program.cs
+++ Parsing Roman Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Parsing_Roman_Numbers
 {
@@ -6,7 +8,55 @@ namespace Parsing_Roman_Numbers
     {
         static void Main(string[] args)
         {
-            
+            var tableNums = new Dictionary<char, int>()
+            {
+                ['I'] = 1,
+                ['V'] = 5,
+                ['X'] = 10,
+                ['L'] = 50,
+                ['C'] = 100,
+                ['D'] = 500,
+                ['M'] = 1000
+            };
+
+
         }
+        static int CollectArabicNumbers()
+        {
+            // TODO: Написать функцию которая соберает результат конвертации римских чисел.
+            return default;
+        }
+
+        static List<int> GetArabicNums()
+        {
+            // TODO: Написать функицю которая используя  ConvertRomanNumsToArabiсNums и
+            //  RecognizeRomansNums получает список арабских чисел.
+            return default;
+        }
+
+        static string RecognizeRomansNums(string romanNums)
+        {
+            // TODO: Написать функцию которая обнаруживает римское число и возвращает его.
+            return default;
+        }
+
+        /* ==========================================================================================
+         *                  Конвертировать римские числа в арабские числа
+         * ==========================================================================================
+        */
+        static int ConvertRomanNumsToArabiсNums(Dictionary<char, int> tableNums, string romanNums)
+        {
+            // TODO: написать функцию которая берет римское число и конвертирует его в арабское.
+            return default;
+        }
+
+        static bool CompareNumbers(int arabianNumbers, int romanNumbers)
+        {
+            // TODO: Реализовать сравнение.
+            return false;
+        }
+       
+
+
     }
 }
